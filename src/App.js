8b0affc5f9ea42aed12c1196414c245efc683ac9
Peelsob4d <peelsob4d@gmail.com>
2022-01-05 21:40:16 +0900

    Testing get(), fixed some errors

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import { useState } from "react";
-import { characters } from "./Data";
-import { profiles } from "./Data";
 
-export default function CharacterList(){
-  return (
-    <>
-      <p>
-        {characters}
-      </p>
-      <p>
-        {profiles}
-      </p>
-    </>
-  );
+export default function InputUID() {
+    let profile;
+    const [ uid, setUID ] = useState('');
+    function handleUIDClick() {
+        localStorage.setItem('UID', uid);
+    }
+    function handleUIDChange(e) {
+        setUID(e.target.value);
+    }
+    fetch(`http://localhost:3030/api/825460402/userdata`)
+    .then(res => res.json())
+    .then(json => profile = json);
+    return (
+        <>
+            <div>
+                <h1>Input your Genshin Impact UID</h1>
+                <label htmlFor="">UID:</label>
+                <input type="text" onChange={handleUIDChange}/>
+                <button onClick={handleUIDClick}>Input</button>
+            </div>
+            <p>{profile}</p>
+        </>
+    )
 }
