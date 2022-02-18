c33a553d2037b488a72e2ed7a3ee04b41e73812c
Peelsob4d <peelsob4d@gmail.com>
2022-02-18 22:06:01 +0900

    Adding MUI, some images and data

---
+++ src/components/DailyData/dailyData.ts
@@ -0,0 +1,29 @@
+export const dailydata = [
+  {
+    id: 0,
+    monde_book: 'img/freedom.png',
+    liyue_book: 'img/prosperity.png',
+    inazuma_book: 'img/transience.png',
+    monde_material: 'img/dream.png',
+    liyue_material: 'img/guyun.png',
+    inazuma_material: 'img/sea.png',
+  },
+  {
+    id: 1,
+    monde: 'img/resistance.png',
+    liyue: 'img/diligence.png',
+    inazuma: 'img/elegance.png',
+    monde_material: 'img/nostalgia.png',
+    liyue_material: 'img/elixir.png',
+    inazuma_material: 'img/valor.png',
+  },
+  {
+    id: 2,
+    monde: 'img/ballad.png',
+    liyue: 'img/gold.png',
+    inazuma: 'img/light.png',
+    monde_material: 'img/gladiator.png',
+    liyue_material: 'img/aerosiderite.png',
+    inazuma_material: 'img/kijin.png',
+  },
+];
--- src/components/PrivateProfile/PrivateProfile.tsx
+++ src/components/PrivateProfile/PrivateProfile.tsx
@@ -6,6 +6,8 @@ import {
 } from '../../features/userresin/userresin';
 import { selectData } from '../../features/userdata/userdata';
 import styles from './PrivateProfile.module.css';
+import Character, { CharacterProps } from './Character';
+import { Grid } from '@mui/material';
 
 function PrivateProfile() {
   const dispatch = useDispatch();
@@ -18,28 +20,51 @@ function PrivateProfile() {
     return () => clearInterval(timer);
   }, []);
   return (
-    <div>
-      <div className={styles.Profile}>
-        <div className={styles.Nickname}>
-          <h2>{data.handbook.nickname}</h2>
-        </div>
-        <div className={styles.Dailynote}>
-          <ul>
-            <li>
-              레진:{' '}
-              {resin !== null ? resin.resin : data.dailynote.current_resin}
-            </li>
-            <li>선계 코인: {data.dailynote.current_home_coin}</li>
-            <li>남은 조사 횟수: {data.dailynote.max_expedition_num}</li>
-            <li>
-              일일 퀘스트:{' '}
-              {data.dailynote.total_task_num - data.dailynote.finished_task_num}
-              /{data.dailynote.total_task_num}
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <Grid container direction='column'>
+      <Grid item>test1</Grid>
+      <Grid item container>
+        <Grid xs={3}>-----</Grid>
+        <Grid xs={6}>test2</Grid>
+        <Grid xs={3}>-----</Grid>
+      </Grid>
+    </Grid>
+    // <div>
+    //   <div className={styles.Profile}>
+    //     <div className={styles.Nickname}>
+    //       <h2>{data.handbook.nickname}</h2>
+    //     </div>
+    //     <div className={styles.Dailynote}>
+    //       <ul>
+    //         <li>
+    //           레진:{' '}
+    //           {resin !== null ? resin.resin : data.dailynote.current_resin}
+    //         </li>
+    //         <li>선계 코인: {data.dailynote.current_home_coin}</li>
+    //         <li>남은 조사 횟수: {data.dailynote.max_expedition_num}</li>
+    //         <li>
+    //           일일 퀘스트:{' '}
+    //           {data.dailynote.total_task_num - data.dailynote.finished_task_num}
+    //           /{data.dailynote.total_task_num}
+    //         </li>
+    //       </ul>
+    //     </div>
+    //     <div className={styles.Stats}>
+    //       <ul>
+    //         <li>접속일: {data.profile.stats.active_day_number}</li>
+    //         <li>달성한 업적 개수: {data.profile.stats.achievement_number}</li>
+    //         <li>아바타 개수: {data.profile.stats.avatar_number}</li>
+    //         <li>이번 나선비경 달성현황: {data.profile.stats.spiral_abyss}</li>
+    //       </ul>
+    //     </div>
+    //     <div className={styles.Dailyfarming}>오늘 비경 소재는..</div>
+    //     <div className={styles.CharacterList}>
+    //       {data.characters.avatars.map((info: any) => (
+    //         <Character key={info.id} data={info} />
+    //       ))}
+    //     </div>
+    //     <div className={styles.CharacterList}></div>
+    //   </div>
+    // </div>
   );
 }
 
