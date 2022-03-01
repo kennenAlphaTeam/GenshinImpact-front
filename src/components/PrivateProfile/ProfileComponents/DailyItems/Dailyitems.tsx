3ae2fde0848369f9c745dc69d8e15d9b1a84889b
Peelsob4d <peelsob4d@gmail.com>
2022-03-02 03:16:47 +0900

    Dailyitems에 CSS적용완료

--- src/components/PrivateProfile/ProfileComponents/DailyItems/Dailyitems.tsx
+++ src/components/PrivateProfile/ProfileComponents/DailyItems/Dailyitems.tsx
@@ -1,37 +1,40 @@
 import React from 'react';
 import { Grid, Box, Container, Typography } from '@mui/material';
 import { dailydata } from './dailyData';
+import styles from './Dailyitems.module.css';
 export default function Dailyitem() {
   const date = new Date().getDate();
   const index = date === 0 ? 0 : date % 3 === 1 ? 0 : date % 3 === 2 ? 1 : 2;
   //0 1a 2b 3c 4a 5b 6c
   return (
-    <Grid container direction='column'>
-      <Grid item>
+    <div className={styles.Box}>
+      <div className={styles.Fontdiv}>
         <Typography variant='inherit' align='left' component='div'>
           지금 얻을 수 있는 소재는...
         </Typography>
-      </Grid>
-      <Grid item container columns={6} spacing={1} margin={1}>
-        <Grid item xs={1}>
-          <img src={dailydata[index].monde_book} alt='' />
+      </div>
+      <div className={styles.Imagediv}>
+        <Grid className={styles.Imagebox} item container columns={6}>
+          <Grid item xs={1}>
+            <img src={dailydata[index].monde_book} alt='' />
+          </Grid>
+          <Grid item xs={1}>
+            <img src={dailydata[index].liyue_book} alt='' />
+          </Grid>
+          <Grid item xs={1}>
+            <img src={dailydata[index].inazuma_book} alt='' />
+          </Grid>
+          <Grid item xs={1}>
+            <img src={dailydata[index].monde_material} alt='' />
+          </Grid>
+          <Grid item xs={1}>
+            <img src={dailydata[index].liyue_material} alt='' />
+          </Grid>
+          <Grid item xs={1}>
+            <img src={dailydata[index].inazuma_material} alt='' />
+          </Grid>
         </Grid>
-        <Grid xs={1}>
-          <img src={dailydata[index].liyue_book} alt='' />
-        </Grid>
-        <Grid item xs={1}>
-          <img src={dailydata[index].inazuma_book} alt='' />
-        </Grid>
-        <Grid item xs={1}>
-          <img src={dailydata[index].monde_material} alt='' />
-        </Grid>
-        <Grid item xs={1}>
-          <img src={dailydata[index].liyue_material} alt='' />
-        </Grid>
-        <Grid item xs={1}>
-          <img src={dailydata[index].inazuma_material} alt='' />
-        </Grid>
-      </Grid>
-    </Grid>
+      </div>
+    </div>
   );
 }
