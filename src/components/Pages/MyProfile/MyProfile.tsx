4c67a5e4d88519c6a86eab4d7228e10a3ebae881
Peelsob4d <peelsob4d@gmail.com>
2022-04-04 22:02:39 +0900

    지역 진행도, 계정 기록 추가

--- src/components/Pages/MyProfile/MyProfile.tsx
+++ src/components/Pages/MyProfile/MyProfile.tsx
@@ -1,14 +1,4 @@
-import styled from '@emotion/styled/types/base';
-import {
-  AppBar,
-  Container,
-  createStyles,
-  Grid,
-  makeStyles,
-  Theme,
-  Toolbar,
-  Typography,
-} from '@mui/material';
+import { AppBar, Grid, Toolbar } from '@mui/material';
 import React from 'react';
 import styles from './MyProfile.module.css';
 
@@ -143,7 +133,147 @@ const TodayMaterials = () => {
 };
 
 const WorldExp = () => {
-  return <></>;
+  return (
+    <Grid container spacing={2} className={styles.Grid}>
+      <Grid item xs></Grid>
+      <Grid item xs={10}>
+        <div className={styles.WorldExpList}>
+          <div className={styles.WorldExpBox}>
+            <div className={styles.WorldBoxFilter}>
+              <div>
+                <img
+                  src='img/icons/Monde.png'
+                  alt=''
+                  className={styles.WorldIcon}
+                />
+                <div className={styles.WorldName}>몬드</div>
+                <div className={styles.WorldLevel}>평판 레벨:8</div>
+                <div className={styles.WorldProgress}>탐사 진행도: 86.9%</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.WorldExpBox}>
+            <div className={styles.WorldBoxFilter}>
+              <div>
+                <img
+                  src='img/icons/Liyue.png'
+                  alt=''
+                  className={styles.WorldIcon}
+                />
+                <div className={styles.WorldName}>리월</div>
+                <div className={styles.WorldLevel}>평판 레벨:8</div>
+                <div className={styles.WorldProgress}>탐사 진행도: 86.9%</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.WorldExpBox}>
+            <div className={styles.WorldBoxFilter}>
+              <div>
+                <img
+                  src='img/icons/Inazuma.png'
+                  alt=''
+                  className={styles.WorldIcon}
+                />
+                <div className={styles.WorldName}>이나즈마</div>
+                <div className={styles.WorldLevel}>평판 레벨:8</div>
+                <div className={styles.WorldProgress}>탐사 진행도: 86.9%</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Grid>
+      <Grid item xs></Grid>
+    </Grid>
+  );
+};
+
+const MyData = () => {
+  return (
+    <Grid container spacing={2} className={styles.Grid}>
+      <Grid item xs />
+      <Grid item xs={8}>
+        <div className={styles.MydataList}>
+          <div className={styles.MydataTextBox}>
+            <div className={styles.MydataText}>
+              <div>245</div>
+              <div>활동 일수</div>
+            </div>
+            <div className={styles.MydataText}>
+              <div>31</div>
+              <div>비경 개방</div>
+            </div>
+          </div>
+          <div className={styles.MydataDivline} />
+          <div className={styles.MydataTextBox}>
+            <div className={styles.MydataText}>
+              <div>344</div>
+              <div>업적 달성 개수</div>
+            </div>
+            <div className={styles.MydataText}>
+              <div>12-3</div>
+              <div>나선 비경</div>
+            </div>
+          </div>
+          <div className={styles.MydataDivline} />
+          <div className={styles.MydataTextBox}>
+            <div className={styles.MydataText}>
+              <div>35</div>
+              <div>획득한 캐릭터</div>
+            </div>
+            <div className={styles.MydataText}>
+              <div>92</div>
+              <div>화려한 보물상자</div>
+            </div>
+          </div>
+          <div className={styles.MydataDivline} />
+          <div className={styles.MydataTextBox}>
+            <div className={styles.MydataText}>
+              <div>159</div>
+              <div>워프 포인트</div>
+            </div>
+            <div className={styles.MydataText}>
+              <div>205</div>
+              <div>진귀한 보물상자</div>
+            </div>
+          </div>
+          <div className={styles.MydataDivline} />
+          <div className={styles.MydataTextBox}>
+            <div className={styles.MydataText}>
+              <div>65</div>
+              <div>바람 신의 눈동자</div>
+            </div>
+            <div className={styles.MydataText}>
+              <div>577</div>
+              <div>정교한 보물상자</div>
+            </div>
+          </div>
+          <div className={styles.MydataDivline} />
+          <div className={styles.MydataTextBox}>
+            <div className={styles.MydataText}>
+              <div>131</div>
+              <div>바위 신의 눈동자</div>
+            </div>
+            <div className={styles.MydataText}>
+              <div>674</div>
+              <div>평범한 보물상자</div>
+            </div>
+          </div>
+          <div className={styles.MydataDivline} />
+          <div className={styles.MydataTextBox}>
+            <div className={styles.MydataText}>
+              <div>180</div>
+              <div>번개 신의 눈동자</div>
+            </div>
+            <div className={styles.MydataText}>
+              <div>14</div>
+              <div>신묘한 보물상자</div>
+            </div>
+          </div>
+        </div>
+      </Grid>
+      <Grid item xs />
+    </Grid>
+  );
 };
 
 const MyProfile = () => {
@@ -158,6 +288,19 @@ const MyProfile = () => {
       <div className={styles.Materials}>
         <div className={styles.MaterialsGrid}>
           <TodayMaterials />
+        </div>
+      </div>
+      <div className={styles.BlackBg}>
+        <div className={styles.Worlds}>
+          <div className={styles.WorldsGrid}>
+            <WorldExp />
+          </div>
+        </div>
+        <div className={styles.Contour}></div>
+        <div className={styles.Myinfo}>
+          <div className={styles.MyinfoGrid}>
+            <MyData />
+          </div>
         </div>
       </div>
     </div>
