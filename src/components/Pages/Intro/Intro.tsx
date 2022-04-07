7ffc7eb73d99b44979cb2868d72ba68e653f0741
Peelsob4d <peelsob4d@gmail.com>
2022-04-07 23:43:31 +0900

    login, Intro페이지 추가

--- src/components/Pages/Intro/Intro.tsx
+++ src/components/Pages/Intro/Intro.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import styles from './Intro.module.css';
 
 const Intro = () => {
   return (
-    <div>
-      <a href='https://localhost:8080/auth/oauth/login/google'>구글 로그인</a>
+    <div className={styles.Body}>
+      <div>
+        <div>
+          <img src='img/banner-tri.png'></img>Kenshin
+        </div>
+        <a href='https://localhost:8080/auth/oauth/login/google'>구글 로그인</a>
+      </div>
     </div>
   );
 };
