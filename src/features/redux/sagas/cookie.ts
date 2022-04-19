b8775e670d69304affa9a247557c767e1fdd88f0
Peelsob4d <peelsob4d@gmail.com>
2022-04-20 02:52:15 +0900

    리다이랙트 오류 수정

--- src/features/redux/sagas/cookie.ts
+++ src/features/redux/sagas/cookie.ts
@@ -5,6 +5,8 @@ import {
   FETCH_COOKIE_REQUEST,
   getMyDailyAsync,
   getMyProfileAsync,
+  GO_TO_INTRO,
+  GO_TO_LOGIN,
   GO_TO_MYPROFILE,
 } from '../constants/actionTypes';
 
@@ -19,7 +21,27 @@ function* fetchCookie(action: ReturnType<typeof fetchCookieAsync.request>) {
     yield put({ type: GO_TO_MYPROFILE });
   } catch (error) {
     yield put(fetchCookieAsync.failure(error));
+    switch (errorSlice(error.message)) {
+      case '500':
+        //서버 에러 => 에러페이지로 보내기
+        break;
+      case '401':
+        //구글 OAuth에러 => 메인페이지로 보내기
+        yield put({ type: GO_TO_INTRO });
+        break;
+      case '400':
+        //잘못된 요청 => 일어날 가능성 x 그냥 로그인페이지로 보내기
+        yield put({ type: GO_TO_LOGIN });
+        break;
+      default:
+        yield put({ type: GO_TO_LOGIN });
+    }
   }
+}
+
+function errorSlice(msg: string): string | undefined {
+  const err_code: string | undefined = msg.split(' ').pop();
+  return err_code;
 }
 
 function* watchFetchCookie() {
